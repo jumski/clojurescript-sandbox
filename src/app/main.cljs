12c6b846cebd5f707ee2846a0f465e1b5f7bff96
Wojtek Majewski <jumski@gmail.com>
2020-03-15 17:08:47 +0100

    Simple table in reagent with buttons changing state

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -2,10 +2,29 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]))
 
-(def state (r/atom "yoflaki"))
+(def rows-num (r/atom 5))
+(def cols-num (r/atom 5))
 
-(defn example-component []
-  [:strong "message: " @state])
+(defn create-cols [num body-fn]
+  (for [i (range num)]
+    ^{:key (body-fn i)} [:td (body-fn i)]))
+
+(defn table []
+  [:table
+   [:tbody
+    (doall
+      (for [rn (range @rows-num)
+            :let [col-body-fn #(str "row=" rn " col=" %)]]
+        ^{:key rn} [:tr (create-cols @cols-num col-body-fn)]))]])
+
+(defn button [btn-name click-handler]
+  [:button {:on-click click-handler} btn-name])
+
+(defn layout []
+  [:div
+   [:div "rows" [button "+" #(swap! rows-num inc)] "/" [button "-" #(swap! rows-num dec)]],
+   [:div "cols" [button "+" #(swap! cols-num inc)] "/" [button "-" #(swap! cols-num dec)]]
+   [table]])
 
 (defn ^:export main! []
-  (rdom/render [example-component] (js/document.getElementById "app")))
+  (rdom/render [layout] (js/document.getElementById "app")))
